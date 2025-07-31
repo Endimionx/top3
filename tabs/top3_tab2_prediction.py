12fc5f7a877d1c763224367d4e592646a5ed6a2b
Endimionx <endimion7@gmail.com>
2025-07-31 23:57:57 +0700

    Update top3_tab2_prediction.py

--- tabs/top3_tab2_prediction.py
+++ tabs/top3_tab2_prediction.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils import top3_preprocessing, top3_ensemble
 from models import top3_lstm_model, top3_transformer_model
-from top3_markov_predictor import top6_markov, top6_markov_order2
+from top3_markov_hybrid import top6_markov, top6_markov_order2
 
 import pandas as pd
 
